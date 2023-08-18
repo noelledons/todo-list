247cd0b0e911c5c9605b8d18bb46d86b0c02f45c
noelledons <noelledonkor@gmail.com>
2023-08-18 21:54:47 +0100

    style updates for main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [username, setUsername] = useState("");
-  const router = useRouter()
+  const router = useRouter();
 
   const handleInputChange = (event) => {
     setUsername(event.target.value);
@@ -14,22 +14,33 @@ export default function Home() {
 
   const handleSubmit = () => {
     if (username.trim() !== "") {
-      router.push(`/todolist?username=${encodeURIComponent(username)}`)
+      router.push(`/todolist?username=${encodeURIComponent(username)}`);
     }
   };
 
   return (
-      <main className={styles.main}>
-      <h1>Welcome to the Name App!</h1>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={username}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleSubmit} disabled={username.trim() === ""}>
-        Submit
-      </button>
-      </main>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.section}>
+          <h1 className={styles.headerStyling}>Insert your name here:</h1>
+        </div>
+        <div className={styles.section} >
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={username}
+            onChange={handleInputChange}
+            className={styles.inputbox}
+          />
+        </div>
+        <div className={styles.section}>
+          <button className={`${styles.buttonStyling} ${username.trim() === "" ? styles.disabledButton : ""}`} onClick={handleSubmit} disabled={username.trim() === ""}>
+            Submit
+          </button>
+        </div>
+      </div>
+
+      
+    </div>
   );
 }
